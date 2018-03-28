1497cb43826281833385fdf0dddd4690d3421b16
mot <motevans412@gmail.com>
2018-03-28 23:24:00 +0800

    add empty test file for hoc

---
+++ src/hoc/UserFriends.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+describe('<UserFriends />', () => {
+    it('显示好友列表', () => {
+
+    })
+})
---
+++ src/hoc/UserMainPage.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+describe('<UserMainPage />', () => {
+    it('显示个人基本信息的页面，以及好友跟群组', () => {
+
+    })
+})
